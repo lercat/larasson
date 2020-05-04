a53b88afe2a623a55b017dce9f1494b96844e779
lercat <ddbieres@gmail.com>
2020-05-04 14:02:07 +0200

    Rajout QuestionnaireController

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class QuestionnaireController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('questionnaire.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'titre' => 'required',
+            'objectif' => 'required'
+        ]);
+
+        return view('questionnaire.store');
+    }
 }
---
+++ resources/views/questionnaire/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Créer un sondage</div>
+
+                <div class="card-body">
+                    <form action="/questionnaires" method="post">
+                        @csrf
+                        <div class="form-group">
+                          <label for="titre">Titre</label>
+                          <input name="titre" type="text" class="form-control" id="titre" aria-describedby="titreHelp" placeholder="Entrez le titre de votre sondage">
+                          <small id="titreHelp" class="form-text text-muted">Donnez à votre questionnaire un titre attrayant.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="objectif">Objectif</label>
+                            <input name="objectif" type="text" class="form-control" id="objectif" aria-describedby="objectfHelp" placeholder="Entrez l'objectif">
+                            <small id="objectifHelp" class="form-text text-muted">Donner un but qui augmentera les réponses.</small>
+                          </div>
+                          <button type="submit" class="btn btn-primary">Créer le questionnaire</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
