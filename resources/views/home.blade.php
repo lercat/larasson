9feedcbf9c54018b8e167be350cd9a14f8c630b6
lercat <ddbieres@gmail.com>
2020-05-04 10:35:55 +0200

    initialisation larasson

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Larasson</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <a href="/questionnaires/create" class="btn btn-dark">Créer un nouveau sondage</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
